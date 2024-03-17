df5e8ea223201141327851e4fb8d00cf3103c0f3
RAJA8790 <94729964+RAJA8790@users.noreply.github.com>
2024-03-17 15:54:11 +0530

    Update s3.tf

--- cls-14-labs/s3.tf
+++ cls-14-labs/s3.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "storage_service" {
   bucket = "my-photos-7410"
-  force_destroy = false # all objects (including any locked objects) should be deleted from the bucket when the bucket is destroyed so that the bucket can be destroyed without error
-  object_lock_enabled = false #Indicates whether this bucket has an Object Lock configuration enabled
+  force_destroy = true # all objects (including any locked objects) should be deleted from the bucket when the bucket is destroyed so that the bucket can be destroyed without error
+  object_lock_enabled = true #Indicates whether this bucket has an Object Lock configuration enabled
   
   tags = {
     Name        = "web_images"
